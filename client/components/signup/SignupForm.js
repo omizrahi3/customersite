d1b813b86c44cc335e0c20132c6b171816323ed5
Odell Mizrahi <odellm81@live.com>
2018-05-18 01:54:25 -0400

    update

---
+++ client/components/signup/SignupForm.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+
+class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Join Chat With</h1>
+
+        <div className="form-group">
+          <label className="control-label">Username</label>
+          <input
+            value={this.state.username}
+            onChange={this.onChange}
+            type="text"
+            name="username"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Email</label>
+          <input
+            value={this.state.email}
+            onChange={this.onChange}
+            type="text"
+            name="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password</label>
+          <input
+            value={this.state.password}
+            onChange={this.onChange}
+            type="text"
+            name="password"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Confirm Password</label>
+          <input
+            value={this.state.passwordConfirmation}
+            onChange={this.onChange}
+            type="text"
+            name="passwordConfirmation"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <button className="btn btn-primary btn-lg">Signup</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default SignupForm;
