d2300fd3b4295e8be2a677d8b5d0c8270368a0f1
Odell Mizrahi <odellm81@live.com>
2018-05-30 23:12:06 -0400

    search with pagination

--- client/components/search/TalentSearch.js
+++ client/components/search/TalentSearch.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Form, Dropdown, List, Image } from "semantic-ui-react";
+import { Form, Dropdown, List, Image, Input, Grid, Button, Icon } from "semantic-ui-react";
 import Pagination from 'semantic-ui-react-button-pagination';
 import TalentLinkCard from '../cards/TalentLinkCard';
 
@@ -12,8 +12,27 @@ class TalentSearch extends React.Component {
     talents: {},
     resultsPerPage: 15,
     TotalCount: 0,
-    offset: 0
+    offset: 0,
+    searchQuery: ''
   };
+
+  apiCall = (apiUrl, requestBody, offset) => {
+    const instance = axios.create({timeout: 3000});
+    instance.defaults.headers.common['token'] = this.props.user.Token;
+    instance.post(apiUrl, requestBody)
+    .then(res => res.data.Response)
+    .then(searchResults => {
+      const { LandingData, TotalCount } = searchResults;
+      const keys = [];
+      const talentsHash = {};
+      LandingData.forEach(talent => {
+        talent.ProfilePictureReference = "/images/man.png";
+        talentsHash[talent.TalentId] = talent;
+        keys.push(talent.TalentId);
+      });
+      this.setState({ keys, talents: talentsHash, TotalCount, offset });
+    })
+  }
 
   GetAppTalentByCategoryWeb = (offset) => {
     const instance = axios.create({timeout: 3000});
@@ -43,7 +62,13 @@ class TalentSearch extends React.Component {
     console.log('TalentSearch did mount');
     console.log(this.props);
 
-    this.GetAppTalentByCategoryWeb(0);
+    const apiUrl = '/api/api/GetAppTalentByCategoryWeb';
+    const requestBody = {
+      CategoryId: this.props.CategoryId,
+      ResultNumberBegin: 0,
+      ResultNumberEnd: this.state.resultsPerPage
+    }
+    this.apiCall(apiUrl, requestBody, 0);
   }
 
   renderTalents = keys => keys.map(key => {
@@ -59,19 +84,84 @@ class TalentSearch extends React.Component {
   })
 
   handleClick = offset => {
-    this.GetAppTalentByCategoryWeb(offset);
+    let apiUrl;
+    let requestBody;
+    if (this.state.searchQuery) {
+      console.log('GetAppTalentBySearch');
+      const name = this.state.searchQuery.split(' ');
+      apiUrl = '/api/api/GetAppTalentBySearch';
+      requestBody = {
+        CategoryId: 'D4C54DC9580E4143B8F9CA0E87EB7996',
+        FirstName: name[0],
+        LastName: name[1],
+        ResultNumberBegin: offset,
+        ResultNumberEnd: this.state.resultsPerPage
+      }
+    } else {
+      console.log('GetAppTalentByCategoryWeb');
+      apiUrl = '/api/api/GetAppTalentByCategoryWeb';
+      requestBody = {
+        CategoryId: this.props.CategoryId,
+        ResultNumberBegin: offset,
+        ResultNumberEnd: this.state.resultsPerPage
+      }
+    }
+    this.apiCall(apiUrl, requestBody, offset);
+  }
+  
+  onChange = (e, data) => {
+    this.setState({searchQuery: data.value})
+  }
+
+  onSubmit = (e, data) => {
+    let apiUrl;
+    let requestBody;
+    if (this.state.searchQuery) {
+      console.log('GetAppTalentBySearch');
+      const name = this.state.searchQuery.split(' ');
+      apiUrl = '/api/api/GetAppTalentBySearch';
+      requestBody = {
+        CategoryId: 'D4C54DC9580E4143B8F9CA0E87EB7996',
+        FirstName: name[0],
+        LastName: name[1],
+        ResultNumberBegin: 0,
+        ResultNumberEnd: this.state.resultsPerPage
+      }
+    } else {
+      console.log('GetAppTalentByCategoryWeb');
+      apiUrl = '/api/api/GetAppTalentByCategoryWeb';
+      requestBody = {
+        CategoryId: this.props.CategoryId,
+        ResultNumberBegin: 0,
+        ResultNumberEnd: this.state.resultsPerPage
+      }
+    }
+    this.apiCall(apiUrl, requestBody, 0);
   }
 
   render() {
     const { CategoryId } = this.props;
     return (
       <div>
-        <Pagination
-          offset={this.state.offset}
-          limit={this.state.resultsPerPage}
-          total={this.state.TotalCount}
-          onClick={(e, props, offset) => this.handleClick(offset)}
-        />
+        <Grid columns='equal'>
+          <Grid.Column width={4}>
+          <Input
+            action={<Button icon='search' onClick={this.onSubmit} />}
+            placeholder='Search...'
+            onChange={this.onChange}
+          />
+          </Grid.Column>
+          <Grid.Column width={3}>
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <Pagination
+              offset={this.state.offset}
+              limit={this.state.resultsPerPage}
+              total={this.state.TotalCount}
+              onClick={(e, props, offset) => this.handleClick(offset)}
+            />
+          </Grid.Column>
+        </Grid>
         {this.state.keys.length > 0 && (
           <div>
             <h1>{`CategoryId: ${CategoryId}`}</h1>
